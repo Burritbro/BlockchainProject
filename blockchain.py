070272db6f57d68ad9583091f1a0ce7126a711d7
Trevor Cole <108247464+Burritbro@users.noreply.github.com>
2023-05-05 10:33:21 -0400

    Add files via upload

---
+++ blockchain.py
@@ -0,0 +1,171 @@
+from hashmap import Hashmap
+
+
+class Transaction():
+    def __init__(self, from_user, to_user, amount):
+        self._from = from_user
+        self._to = to_user
+        self._amount = amount
+
+    def __hash__(self):
+        # Concatenate the string representation of the attributes
+        str_repr = f"{self._from}{self._to}{self._amount}"
+        # Compute the hash value of the concatenated string
+        return hash(str_repr)
+
+
+class Block():
+    def __init__(self, transactions=None, prev_hash=None):
+        self.prev_hash = prev_hash
+        self.transactions = transactions
+
+    def add_transaction(self, _to, _from, _amount):
+        """adds transactions to the block transaction attribute"""
+        self.transactions.append(Transaction(_to, _from, _amount))
+
+    def __eq__(self, other):
+        """eq method to check if the prev_hash is equal to hash of previous"""
+        if isinstance(other, Block):
+            return self.prev_hash == other.__hash__()
+        return False
+
+    def __hash__(self):
+        """to hash self"""
+        return hash(str(self))
+
+    def __str__(self):
+        """Output formatting function"""
+        return f"previous hash: {self.prev_hash}, transactions: {self.transactions}, hash: {str(id(self))}"
+
+
+class Ledger():
+    def __init__(self):
+        self.balances_hashmap = Hashmap()
+
+    # def has_funds(self, user, amount):
+
+    def has_funds(self, user, amount):
+        """Checking if the user has enough funds for transaction"""
+        #     if user not in self._hashmap:
+
+        if not self.balances_hashmap.get_value(user):
+            return False
+
+        balance = self.balances_hashmap.get_value(user)
+        if self.balances_hashmap.get_value(user) is not None and self.balances_hashmap.get_value(user) >= amount:
+            return True
+        else:
+            return False
+
+
+
+    def deposit(self, user, amount):
+        """deposit funds """
+        self.balances_hashmap._deposit(user, amount)
+
+    def withdraw(self, user_from, amount):
+        """Withdrawing funds functionality"""
+        self.balances_hashmap._withdraw(user_from, amount)
+
+
+class Blockchain():
+    '''Contains the chain of blocks.'''
+
+    #########################
+    # Do not use these three values in any code that you write. 
+    _ROOT_BC_USER = "ROOT"  # Name of root user account.
+    _BLOCK_REWARD = 1000  # Amount of HuskyCoin given as a reward for mining a block
+    _TOTAL_AVAILABLE_TOKENS = 999999  # Total balance of HuskyCoin that the ROOT user receives in block0
+
+    #########################
+
+    def __init__(self):
+        self._blockchain = list()  # Use the Python List for the chain of blocks
+        self._bc_ledger = Ledger()  # The ledger of HuskyCoin balances
+        # Create the initial block0 of the blockchain, also called the "genesis block"
+        self._create_genesis_block()
+
+    # This method is complete. No additional code needed.
+    def _create_genesis_block(self):
+        '''Creates the initial block in the chain.
+        This is NOT how a blockchain usually works, but it is a simple way to give the
+        Root user HuskyCoin that can be subsequently given to other users'''
+        trans0 = Transaction(self._ROOT_BC_USER, self._ROOT_BC_USER, self._TOTAL_AVAILABLE_TOKENS)
+        block0 = Block([trans0])
+        self._blockchain.append(block0)
+        self._bc_ledger.deposit(self._ROOT_BC_USER, self._TOTAL_AVAILABLE_TOKENS)
+
+    # This method is complete. No additional code needed.
+    def distribute_mining_reward(self, user):
+        '''
+        You need to give HuskyCoin to some of your users before you can transfer HuskyCoing
+        between users. Use this method to give your users an initial balance of HuskyCoin.
+        (In the Bitcoin network, users compete to solve a meaningless mathmatical puzzle.
+        Solving the puzzle takes a tremendious amount of copmputing power and consuming a lot
+        of energy. The first node to solve the puzzle is given a certain amount of Bitcoin.)
+        In this assigment, you do not need to understand "mining." Just use this method to 
+        provide initial balances to one or more users.'''
+        trans = Transaction(self._ROOT_BC_USER, user, self._BLOCK_REWARD)
+        block = Block([trans])
+        self.add_block(block)
+
+    def add_block(self, block):
+        """adds block to the blockchain paramater--
+        transction: type list of Transactions"""
+        # Check if there is a transaction
+
+        if not block.transactions:
+            return False
+
+        if block.transactions is not None:
+            prev_hash = self._blockchain[-1].__hash__() if self._blockchain else None
+            block.prev_hash = prev_hash
+            self._blockchain.append(block)
+
+        for transaction in block.transactions:
+            sender = transaction._from
+            receiver = transaction._to
+            amount = transaction._amount
+
+            if not self._bc_ledger.has_funds(sender, amount):
+                return False
+
+            self._bc_ledger.withdraw(sender, amount)
+
+            self._bc_ledger.deposit(receiver, amount)
+
+        return True
+        # self._bc_ledger.balances_hashmap.set_value()
+
+    def validate_chain(self):
+        """Validates that the blocks and hashes are correctly hashed and transactions are possible"""
+
+        validated = True  # validate flag
+        list_tampered = []  # list for blocks tampered with
+        for i in range(1, len(self._blockchain)):
+            if self._blockchain[i].prev_hash != self._blockchain[i - 1].__hash__():  # checking if the hash had changed
+                list_tampered.append(self._blockchain[i])
+                validated = False
+
+
+        if len(list_tampered) > 0:
+            return list_tampered, validated
+        else:
+            return validated
+
+    def __str__(self):
+        """outsputs block in readable form"""
+        block_descriptions = []
+        for block in self._blockchain:
+            block_descriptions.append(f"--previous hash: {block.prev_hash}, transactions: {block.transactions}, hash: {block.__hash__()}")
+        return "".join(block_descriptions)
+
+
+
+chain = Blockchain()
+
+block = Block()
+
+chain.add_block(block)
+
+print(chain)
---
+++ hashmap.py
@@ -0,0 +1,91 @@
+
+class Hashmap:
+    def __init__(self):
+        self._n_buckets = 64
+        self._len = 64
+        self._mapping = self.create_buckets()
+
+    def create_buckets(self):
+        return [dict() for i in range(self._n_buckets)]
+
+    def set_value(self, key, value):
+
+        hashed_key = hash(key) % self._len
+
+        bucket = self._mapping[hashed_key]
+
+        # found_key = False
+
+        for index, record in enumerate(bucket):
+
+            if index == key:
+                # found_key = True
+                break
+
+        # if found_key:
+
+        bucket[key] = value
+        # else:
+        #     bucket[key] = value
+
+
+    def get_value(self, key):
+        hashed_key = hash(key) % self._len
+
+        bucket = self._mapping[hashed_key]
+
+        return bucket[key]
+
+        # found_key = False
+
+        # for index, record in enumerate(bucket):
+        #     searching_key, searching_val = record
+        #
+        #     if searching_key == key:
+        #         found_key = True
+        #         break
+        #
+        # if found_key:
+        #     return searching_val
+        # else:
+        #     return "No value found"
+
+    def delete_val(self, key):
+        """functionality for delete"""
+        hashed_key = hash(key) % self._len
+
+        bucket = self._mapping[hashed_key]
+
+        del bucket[key]
+
+    def _deposit(self, key, amount):
+        """Deposit funds functionality"""
+        hashed_key = hash(key) % self._len
+        if key in self._mapping[hashed_key]:
+            self._mapping[hashed_key][key] += amount
+        else:
+            self._mapping[hashed_key][key] = amount
+
+    def _withdraw(self, key, amount):
+        """functionality for withdrawal"""
+        hashed_key = hash(key) % self._len
+        bucket = self._mapping[hashed_key]
+        if key in bucket:
+            bucket[key] -= amount
+            return f"Balance after transaction: {bucket[key]}"
+        else:
+            return f"Key {key} not found in the hash map."
+
+
+
+
+    def __str__(self):
+        return "".join(str(item) for item in self._mapping)
+
+
+
+
+
+
+
+
